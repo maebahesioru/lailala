e79cd140d0abb6b8e10d77ec7faa612ba7bae725
maebahesioru <pikomaeba@gmail.com>
2026-05-02 14:26:59 +0900

    fix: cast TVHTML5 as any to fix type error in build

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -33,7 +33,7 @@ export async function getInnertubeWithAuth(credentialId: string) {
     const tokens = JSON.parse(raw);
     const innertube = await Innertube.create({
       cache: new UniversalCache(false),
-      client_type: "TVHTML5",
+      client_type: "TVHTML5" as any,
     });
     await innertube.session.signIn(tokens);
     return innertube;
