43b648261e4ec2497dfa67c6d3c8cbee97ee5417
maebahesioru <pikomaeba@gmail.com>
2026-05-02 14:22:27 +0900

    fix: use TVHTML5 instead of TV for client_type

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -33,7 +33,7 @@ export async function getInnertubeWithAuth(credentialId: string) {
     const tokens = JSON.parse(raw);
     const innertube = await Innertube.create({
       cache: new UniversalCache(false),
-      client_type: "TV",
+      client_type: "TVHTML5",
     });
     await innertube.session.signIn(tokens);
     return innertube;
